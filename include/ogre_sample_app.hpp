417d052e305d65b2acd173d6d7f0947342f0af58
zz <zz@test.com>
2021-04-12 09:04:48 +0800

    added but now not working

--- include/ogre_sample_app.hpp
+++ include/ogre_sample_app.hpp
@@ -14,12 +14,12 @@
 #include <iostream>
 
 #include "OgreApplicationContext.h"
-
+#include "OgreTrays.h"
 #include "OgreRoot.h"
 #include "OgreOverlaySystem.h"
 #include "OgreResourceManager.h"
 #include "OgreComponents.h"
-
+#include "OgreCameraMan.h"
 #include "OgreInput.h"
 
 #include <OgreConfigFile.h>
@@ -35,16 +35,24 @@
 #include "OgreInput.h"
 
 
+using namespace Ogre;
+using namespace OgreBites;
+
 class COgreSampleApp : public OgreBites::ApplicationContext,
-						public OgreBites::InputListener
+						public OgreBites::InputListener, public TrayListener
 {
 	public:
 
 		COgreSampleApp():
 			OgreBites::ApplicationContext("OgreSampleApp"){}
 
+		virtual void setupView();
+		virtual void setDragLook(bool enabled);
+		virtual void checkBoxToggled(CheckBox* box);
 		virtual bool keyPressed(const OgreBites::KeyboardEvent& evt);
-
+		virtual void setupParticles();
+		virtual void setupTogglers();
+		virtual void setupContent();
 		virtual void frameRendered(const Ogre::FrameEvent& evt)
 		{
 			std::cout << "frameRendered " << std::endl;
@@ -53,7 +61,17 @@ class COgreSampleApp : public OgreBites::ApplicationContext,
 		virtual void setup();
 
 	private:
+		Ogre::SceneManager* scnMgr = nullptr;
 
+		Ogre::Viewport* mViewport = nullptr;          // main viewport
+		Ogre::Camera* mCamera = nullptr;              // main camera
+		Ogre::SceneNode* mCameraNode = nullptr;       // camera node
+		Ogre::RenderWindow* mWindow = nullptr;      // context render window
+		TrayManager* mTrayMgr = nullptr;           // tray interface manager
+		CameraMan* mCameraMan = nullptr;           // basic camera controller
+		bool mCursorWasVisible = false;             // was cursor visible before dialog appeared
+		bool mDragLook = false;                     // click and drag to free-look
+		SceneNode* mFountainPivot = nullptr;
 };
 
 #endif /* OGRE_SAMPLE_APP_HPP_ */
--- src/ogre_sample_app.cpp
+++ src/ogre_sample_app.cpp
@@ -7,8 +7,111 @@
 #include "OgreCamera.h"
 #include "OgreRenderWindow.h"
 #include "OgreEntity.h"
+#include "OgreParticleSystem.h"
 
-Ogre::SceneManager* scnMgr = nullptr;
+
+void COgreSampleApp::setupView()
+{
+	// setup default viewport layout and camera
+	mCamera = scnMgr->createCamera("MainCamera");
+	mCameraNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+	mCameraNode->attachObject(mCamera);
+	mCameraNode->setFixedYawAxis(true);
+	mViewport = mWindow->addViewport(mCamera);
+	mCamera->setAspectRatio((Ogre::Real)mViewport->getActualWidth() / (Ogre::Real)mViewport->getActualHeight());
+	mCamera->setAutoAspectRatio(true);
+	mCamera->setNearClipDistance(5);
+
+	mCameraMan = new CameraMan(mCameraNode);   // create a default camera controller
+}
+
+void COgreSampleApp::setDragLook(bool enabled)
+{
+	if (enabled)
+	{
+		mCameraMan->setStyle(CS_MANUAL);
+		mTrayMgr->showCursor();
+		mDragLook = true;
+	}
+	else
+	{
+		mCameraMan->setStyle(CS_FREELOOK);
+		mTrayMgr->hideCursor();
+		mDragLook = false;
+	}
+}
+
+void COgreSampleApp::checkBoxToggled(CheckBox* box)
+{
+	// show or hide the particle system with the same name as the check box
+	scnMgr->getParticleSystem(box->getName())->setVisible(box->isChecked());
+}
+
+void COgreSampleApp::setupParticles()
+{
+	ParticleSystem::setDefaultNonVisibleUpdateTimeout(5);  // set nonvisible timeout
+
+	ParticleSystem* ps;
+
+	// create some nice fireworks and place it at the origin
+	ps = scnMgr->createParticleSystem("Fireworks", "Examples/Fireworks");
+	scnMgr->getRootSceneNode()->attachObject(ps);
+
+	// create a green nimbus around the ogre head
+	ps = scnMgr->createParticleSystem("Nimbus", "Examples/GreenyNimbus");
+	scnMgr->getRootSceneNode()->attachObject(ps);
+
+	ps = scnMgr->createParticleSystem("Rain", "Examples/Rain");  // create a rainstorm
+	ps->fastForward(5);   // fast-forward the rain so it looks more natural
+	scnMgr->getRootSceneNode()->createChildSceneNode(Vector3(0, 1000, 0))->attachObject(ps);
+
+	// create aureola around ogre head perpendicular to the ground
+	ps = scnMgr->createParticleSystem("Aureola", "Examples/Aureola");
+	scnMgr->getRootSceneNode()->attachObject(ps);
+
+	// create shared pivot node for spinning the fountains
+	mFountainPivot = scnMgr->getRootSceneNode()->createChildSceneNode();
+
+	ps = scnMgr->createParticleSystem("Fountain1", "Examples/PurpleFountain");  // create fountain 1
+	// attach the fountain to a child node of the pivot at a distance and angle
+	mFountainPivot->createChildSceneNode(Vector3(200, -100, 0), Quaternion(Degree(20), Vector3::UNIT_Z))->attachObject(ps);
+
+	ps = scnMgr->createParticleSystem("Fountain2", "Examples/PurpleFountain");  // create fountain 2
+	// attach the fountain to a child node of the pivot at a distance and angle
+	mFountainPivot->createChildSceneNode(Vector3(-200, -100, 0), Quaternion(Degree(-20), Vector3::UNIT_Z))->attachObject(ps);
+}
+
+void COgreSampleApp::setupTogglers()
+{
+	// create check boxes to toggle the visibility of our particle systems
+	mTrayMgr->createLabel(TL_TOPLEFT, "VisLabel", "Particles");
+	mTrayMgr->createCheckBox(TL_TOPLEFT, "Fireworks", "Fireworks", 130)->setChecked(true);
+	mTrayMgr->createCheckBox(TL_TOPLEFT, "Fountain1", "Fountain A", 130)->setChecked(true);
+	mTrayMgr->createCheckBox(TL_TOPLEFT, "Fountain2", "Fountain B", 130)->setChecked(true);
+	mTrayMgr->createCheckBox(TL_TOPLEFT, "Aureola", "Aureola", 130)->setChecked(false);
+	mTrayMgr->createCheckBox(TL_TOPLEFT, "Nimbus", "Nimbus", 130)->setChecked(false);
+	mTrayMgr->createCheckBox(TL_TOPLEFT, "Rain", "Rain", 130)->setChecked(false);
+}
+void COgreSampleApp::setupContent()
+{
+	// setup some basic lighting for our scene
+	scnMgr->setAmbientLight(ColourValue(0.3, 0.3, 0.3));
+	scnMgr->getRootSceneNode()
+		->createChildSceneNode(Vector3(20, 80, 50))
+		->attachObject(scnMgr->createLight());
+
+	// set our camera to orbit around the origin and show cursor
+	mCameraMan->setStyle(CS_ORBIT);
+	mCameraMan->setYawPitchDist(Degree(0), Degree(15), 250);
+	mTrayMgr->showCursor();
+
+	// create an ogre head entity and place it at the origin
+	Entity* ent = scnMgr->createEntity("Head", "ogrehead.mesh");
+	scnMgr->getRootSceneNode()->attachObject(ent);
+
+	setupParticles();   // setup particles
+	setupTogglers();    // setup particle togglers
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 bool COgreSampleApp::keyPressed(const OgreBites::KeyboardEvent& evt)
@@ -44,7 +147,7 @@ void COgreSampleApp::setup(void)
 {
 	// do not forget to call the base first
 	OgreBites::ApplicationContext::setup();
-
+	mWindow = getRenderWindow();
 	std::cout << "windows list size " << this->mWindows.size() << std::endl;
 
 	// register for input events
@@ -64,20 +167,25 @@ void COgreSampleApp::setup(void)
 	lightNode->setPosition(0, 10, 15);
 	lightNode->attachObject(light);
 	// also need to tell where we are
-	Ogre::SceneNode* camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
-	camNode->setPosition(0, 0, 15);
-	camNode->lookAt(Ogre::Vector3(0, 0, -1), Ogre::Node::TS_PARENT);
-	// create the camera
-	Ogre::Camera* cam = scnMgr->createCamera("myCam");
-	cam->setNearClipDistance(5); // specific to this sample
-	cam->setAutoAspectRatio(true);
-	camNode->attachObject(cam);
-	// and tell it to render into the main window
-	getRenderWindow()->addViewport(cam);
+	//Ogre::SceneNode* camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+	//camNode->setPosition(0, 0, 15);
+	//camNode->lookAt(Ogre::Vector3(0, 0, -1), Ogre::Node::TS_PARENT);
+	//// create the camera
+	//Ogre::Camera* cam = scnMgr->createCamera("myCam");
+	//cam->setNearClipDistance(5); // specific to this sample
+	//cam->setAutoAspectRatio(true);
+	//camNode->attachObject(cam);
+	//// and tell it to render into the main window
+	//getRenderWindow()->addViewport(cam);
+
+    setupView();
+	mTrayMgr = new TrayManager("SampleControls", mWindow, this);  // create a tray interface
 	// finally something to render
 	Ogre::Entity* ent = scnMgr->createEntity("Sinbad.mesh");
 	Ogre::SceneNode* node = scnMgr->getRootSceneNode()->createChildSceneNode();
 	node->attachObject(ent);
+
+	setupContent();
 
 
 	OgreBites::Event evt;
